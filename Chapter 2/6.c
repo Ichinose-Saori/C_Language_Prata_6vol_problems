8343ae7b5d32336628da294f742e91cc1d04da06
Ichinose_Saori <ichinose.saori.lab@gmail.com>
2024-06-14 07:23:07 +0900

    Problem 2.6

---
+++ Chapter 2/6.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int toes = 10;
+    printf("Toes original value is %d. The doubled toes is %d. THe square of toes is %d.\n", toes, 2*toes, toes*toes);
+}
