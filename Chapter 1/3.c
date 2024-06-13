af92f116c50991249733f61649335a45c180a484
Ichinose_Saori <ichinose.saori.lab@gmail.com>
2024-06-13 17:33:44 +0900

    Problem 2.3

---
+++ Chapter 1/3.c
@@ -0,0 +1,9 @@
+//this program 	translates your age from years to days 
+#include <stdio.h>
+
+int main()
+{
+	int age = 26;
+	printf("Your age is 26! If we were to count it in days, it would be %d days.\n", age*365);
+}
+
