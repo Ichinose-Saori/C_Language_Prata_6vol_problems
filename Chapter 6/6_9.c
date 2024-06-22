93cf11f180448edd8180ce9ec98d95ce6bd2d841
Saori <ichinose.saori.lab@gmail.com>
2024-06-22 09:46:06 +0900

    Update 6_9.c

--- Chapter 6/6_9.c
+++ Chapter 6/6_9.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 double absol(double a, double b); //abs() function from math.h
-void manip(double a, double b); //function from exercise 6_9
+void manip(double a, double b);
 
 int main()
 {
@@ -21,8 +21,11 @@ double absol(double a, double b)
     if (a>b) return a-b;
     return b-a;
 }
-
 void manip(double a, double b)
 {
-    printf("%lf\n", absol(a, b)/(a*b));
+    if (a==0 || b == 0)
+    {
+            printf("Ehh, zero numbers are prohibited, try again!\n");
+    }
+    else printf("%lf\n", absol(a, b)/(a*b));
 }
