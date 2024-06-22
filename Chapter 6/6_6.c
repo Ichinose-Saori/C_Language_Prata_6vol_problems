ede1616bd92be5ebae5869d043672ebaa24d2794
Saori <ichinose.saori.lab@gmail.com>
2024-06-22 09:56:26 +0900

    Update 6_6.c

--- Chapter 6/6_6.c
+++ Chapter 6/6_6.c
@@ -3,13 +3,28 @@
 int main()
 {
     long long downB, upB;
-    scanf("%lld", &downB);
-    scanf("%lld", &upB);
+    int program_is_down = 0;
+
+    if (scanf("%lld", &downB)!=1 || scanf("%lld", &upB)!=1){
+                printf("You broke the program, next time don't input noninteger values!\n");
+        program_is_down = 1;
+        return -1;
+    }
+
+    while(downB>upB)
+    {
+        printf("You entered incorrect data, try again!\n");
+        if (scanf("%lld", &downB)!=1 || scanf("%lld", &upB)!=1){
+                printf("You broke the program, next time don't input noninteger values!\n");
+        program_is_down = 1;
+        return -1;
+        }
+    }
 
     for (long long i = downB; i<=upB; i++)
     {
         printf("%lld %lld %lld\n", i, i*i, i*i*i);
     }
+
     return 0;
 }
-
