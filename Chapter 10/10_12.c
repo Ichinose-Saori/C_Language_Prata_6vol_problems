f17432616ab15005f76bfcf69556d34e89b2e093
Saori <ichinose.saori.lab@gmail.com>
2024-07-02 11:46:48 +0900

    Create 10_12.c

---
+++ Chapter 10/10_12.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#define MONTHS 12
+#define YEARS 5
+
+void avg_precip(float (*rain)[MONTHS], int rows);
+void avg_precip_mon(float (*rain)[MONTHS], int rows);
+
+int main(void)
+{
+    const float rain[YEARS][MONTHS] =
+    {
+        {4.3, 4.3, 4.3, 3.0, 2.0, 1.2, 0.2, 0.2, 0.4, 2.4, 3.5, 6.6},
+        {8.5, 8.2, 1.2, 1.6, 2.4, 0.0, 5.2, 0.9, 0.3, 0.9, 1.4, 7.3},
+        {9.1, 8.5, 6.7, 4.3, 2.1, 0.8, 0.2, 0.2, 1.1, 2.3, 6.1, 8.4},
+        {7.2, 9.9, 8.4, 3.3, 1.2, 0.8, 0.4, 0.0, 0.6, 1.7, 4.3, 6.2},
+        {7.6, 5.6, 3.8, 2.8, 3.8, 0.2, 0.0, 0.0, 0.0, 1.3, 2.6, 5.2}
+    };
+    avg_precip(rain, YEARS);
+    avg_precip_mon(rain, YEARS);
+    return 0;
+}
+
+void avg_precip(float (*rain)[MONTHS], int rows)
+{
+    int year, month;
+    float subtot, total;
+    printf("YEAR      AMOUNT OF PRECIPITATION (in inches)\n");
+    for (year = 0, total = 0; year<YEARS; year++)
+    {
+        for (month = 0, subtot = 0; month < MONTHS; month++)
+            subtot+=*(*(rain+year)+month);
+        printf("%5d %15.1f\n", 2010 + year, subtot);
+        total+=subtot;
+    }
+
+    printf("\nAverage precipitation is %.1f inches.\n\n",total/YEARS);
+}
+void avg_precip_mon(float (*rain)[MONTHS], int rows)
+{
+    int year, month;
+    float subtot, total;
+    printf("Average precipitation per month\n\n");
+    printf(" Jan  Feb  Mar  Apr  May  Jun  Jul  Aug  Sep  Oct  Nov  Dec\n");
+    for (month = 0; month<MONTHS; month++)
+    {
+        for (year = 0, subtot = 0; year < YEARS; year++)
+            subtot+= *(*(rain+year)+month);
+        printf("%4.1f ", subtot/YEARS);
+    }
+}
