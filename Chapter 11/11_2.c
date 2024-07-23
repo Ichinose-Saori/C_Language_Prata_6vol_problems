b39efc0fe4ead32d34d49b2939a9a13fc92348f3
Saori <ichinose.saori.lab@gmail.com>
2024-07-23 21:41:48 +0900

    Create 11_2.c

---
+++ Chapter 11/11_2.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#define SIZE 5
+
+void grave_danger(char*, int);
+
+int main(void)
+{
+	int arr[SIZE+1];
+	grave_danger(arr, SIZE);
+	fputs(arr, stdout);
+	return 0;
+}
+
+void grave_danger(char* arr, int n)
+{
+	int i = 0; 
+	while (i < n)
+	{
+		arr[i] = getchar();
+		if (arr[i] == ' ' || arr[i] == '\n' || arr[i] == '\t')
+		{
+			arr[i] = '\0';
+			return;
+		}
+		i++;
+	}
+	arr[n] = '\0';
+}
